525b984fa229f6b704885d4f7c4bff257b9e4bd4
Yasith Eranda Yapa Amarasekara <106188847+yashEra@users.noreply.github.com>
2023-12-19 20:44:18 +0530

    Update

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,7 +1,7 @@
 import recat,{useState} from "react";
 import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faMagnifyingGlass, faCartShopping, faHeart, faEnvelope } from '@fortawesome/free-solid-svg-icons'
+import { faMagnifyingGlass, faCartShopping, faHeart, faUser } from '@fortawesome/free-solid-svg-icons'
 
 const NavBar = () => {
   const [nav, setNav] = useState(false)
@@ -9,14 +9,14 @@ const NavBar = () => {
     setNav(!nav)
   }
   return (
-    <div className="w-full h-[90px] bg-black " >
+    <div className="w-full h-[90px] bg-while " >
       <div className="max-w-[1240px] mx-auto px-4 flex justify-between items-center h-full">
         <div>
-          <h1 className="text-[#1357DE]">BidWiz 2.0</h1>
+          <h1 className="text-[#1357DE] font-black">BidWiz 2.0</h1>
         </div>
         <div>
-          <div className="hidden md:flex">
-            <ul className="flex text-[#f9ffff] items-center">
+          <div className="hidden md:flex font-bold">
+            <ul className="flex text-[#555555] items-center">
               <li>Catogories</li>
               <li>About US</li>
               <li>Contact Us</li>
@@ -25,9 +25,9 @@ const NavBar = () => {
           </div>
           
           <div className="block md:hidden">
-            {nav ? <AiOutlineClose onClick={handleNav} size={30} className="text-[#f9ffff]" /> :  <AiOutlineMenu onClick={handleNav} size={30} className="text-[#f9ffff]" /> }
+            {nav ? <AiOutlineClose onClick={handleNav} size={30} className="text-[#1357DE]" /> :  <AiOutlineMenu onClick={handleNav} size={30} className="text-[#1357DE]" /> }
           </div>
-          <div className={nav ? "w-full bg-black text-[#f9ffff] absolute top-[89px] left-0 flex justify-center text-center" : "absolute left-[-100%]"}>
+          <div className={nav ? "w-full bg-black text-[#f9ffff] font-bold absolute top-[89px] left-0 flex justify-center text-center" : "absolute left-[-100%]"}>
             <ul>
               <li className="text-2xl">Home</li>
               <li className="text-2xl">Services</li>
@@ -39,8 +39,8 @@ const NavBar = () => {
           </div>
         </div>
         <div className="hidden md:flex">
-            <ul className="flex text-[#f9ffff] items-center">
-              <li>Login/Register</li>
+            <ul className="flex text-[#1357DE] items-center">
+              <li><FontAwesomeIcon icon={faUser} /><span  className="px-4 font-bold">Login/Register</span></li>
               <li><FontAwesomeIcon icon={faMagnifyingGlass} /></li>
               <li><FontAwesomeIcon icon={faCartShopping} /></li>
               <li><FontAwesomeIcon icon={faHeart} /></li>
