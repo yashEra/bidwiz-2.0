f5ae79ce43ad300c6f209f10b2442bb750509f52
Yasith Eranda Yapa Amarasekara <106188847+yashEra@users.noreply.github.com>
2023-12-24 11:16:00 +0530

    update

--- src/components/home/TopSection.jsx
+++ src/components/home/TopSection.jsx
@@ -5,14 +5,38 @@ import './assest/css/home.css'
 const TopSection = () => {
   return (
     <div
-      className="w-[80.20833333333333%] bg-[white] top-sec-main"
+      className="w-[80.20833333333333%] bg-[white] top-sec-main drop-shadow-xl"
     >
       <div className="flex lg:flex-row sm:flex-col md:flex-col justify-around items-center">
 
           <div>
-            <a className="flex flex-col justify-center items-center sm:p-8 md:p-8">
-              <img src={MobileIcon} alt="" width='90px'/>
-              <span className="text-[--primary-blue] text-2xl font-bold">
+            <a className="flex flex-col justify-center items-center p-8">
+              <img src={MobileIcon} alt="" width='60px'/>
+              <span className="text-[--primary-blue] text-xl font-bold">
+                Mobile Devices
+              </span>
+              <span className="text-[--primary-blue] text-md">
+                Mobile Devices
+              </span>
+            </a>
+          </div>
+
+          <div>
+            <a className="flex flex-col justify-center items-center p-8">
+              <img src={MobileIcon} alt="" width='60px'/>
+              <span className="text-[--primary-blue] text-xl font-bold">
+                Mobile Devices
+              </span>
+              <span className="text-[--primary-blue] text-md">
+                Mobile Devices
+              </span>
+            </a>
+          </div>
+
+          <div>
+            <a className="flex flex-col justify-center items-center p-8">
+              <img src={MobileIcon} alt="" width='60px'/>
+              <span className="text-[--primary-blue] text-xl font-bold">
                 Mobile Devices
               </span>
               <span className="text-[--primary-blue] text-lg">
@@ -22,36 +46,12 @@ const TopSection = () => {
           </div>
 
           <div>
-            <a className="flex flex-col justify-center items-center p-4">
-              <img src={MobileIcon} alt="" width='90px'/>
-              <span className="text-[--primary-blue] text-2xl font-bold">
+            <a className="flex flex-col justify-center items-center p-8">
+              <img src={MobileIcon} alt="" width='60px'/>
+              <span className="text-[--primary-blue] text-xl font-bold">
                 Mobile Devices
               </span>
-              <span className="text-[--primary-blue] text-lg">
-                Mobile Devices
-              </span>
-            </a>
-          </div>
-
-          <div>
-            <a className="flex flex-col justify-center items-center p-4">
-              <img src={MobileIcon} alt="" width='90px'/>
-              <span className="text-[--primary-blue] text-2xl font-bold">
-                Mobile Devices
-              </span>
-              <span className="text-[--primary-blue] text-lg">
-                Mobile Devices
-              </span>
-            </a>
-          </div>
-
-          <div>
-            <a className="flex flex-col justify-center items-center p-4">
-              <img src={MobileIcon} alt="" width='90px'/>
-              <span className="text-[--primary-blue] text-2xl font-bold">
-                Mobile Devices
-              </span>
-              <span className="text-[--primary-blue] text-lg">
+              <span className="text-[--primary-blue] text-md">
                 Mobile Devices
               </span>
             </a>
