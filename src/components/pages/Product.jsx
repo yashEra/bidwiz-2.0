0e77b16e8b028ee24c3adc6aa3657087b5249198
Yasith Eranda Yapa Amarasekara <106188847+yashEra@users.noreply.github.com>
2023-12-29 12:42:02 +0530

    update

--- src/components/pages/Product.jsx
+++ src/components/pages/Product.jsx
@@ -8,10 +8,10 @@ import Footer from "../footer/Footer";
 
 const ProductDes = () => {
   const [images, setImages] = useState({
-    img1: "https://static.nike.com/a/images/t_PDP_1280_v1/f_auto,b_rgb:f5f5f5/3396ee3c-08cc-4ada-baa9-655af12e3120/scarpa-da-running-su-strada-invincible-3-xk5gLh.png",
-    img2: "https://static.nike.com/a/images/f_auto,b_rgb:f5f5f5,w_440/e44d151a-e27a-4f7b-8650-68bc2e8cd37e/scarpa-da-running-su-strada-invincible-3-xk5gLh.png",
-    img3: "https://static.nike.com/a/images/f_auto,b_rgb:f5f5f5,w_440/44fc74b6-0553-4eef-a0cc-db4f815c9450/scarpa-da-running-su-strada-invincible-3-xk5gLh.png",
-    img4: "https://static.nike.com/a/images/f_auto,b_rgb:f5f5f5,w_440/d3eb254d-0901-4158-956a-4610180545e5/scarpa-da-running-su-strada-invincible-3-xk5gLh.png",
+    img1: "https://images.unsplash.com/photo-1468436139062-f60a71c5c892?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    img2: "https://images.unsplash.com/photo-1517336714731-489689fd1ca8?q=80&w=1452&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    img3: "https://images.unsplash.com/photo-1541807084-5c52b6b3adef?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    img4: "https://images.unsplash.com/photo-1611186871348-b1ce696e52c9?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   });
 
   const [activeImg, setActiveImage] = useState(images.img1);
@@ -21,37 +21,37 @@ const ProductDes = () => {
   return (
     <>
       <NavBar />
-      <div className="max-w-7xl lg:pt-[10%] md:pt-[15%] pt-[20%] pb-[5%] px-[9.895833333333333%]">
+      <div className="w-full lg:pt-[10%] md:pt-[15%] pt-[20%] pb-[5%] px-[9.895833333333333%]">
         <div className="flex flex-col justify-between lg:flex-row gap-16 lg:items-center">
           <div className="flex flex-col gap-6 lg:w-2/4">
             <img
               src={activeImg}
               alt=""
-              className="w-full h-full aspect-square object-cover rounded-xl"
+              className="w-full h-full aspect-square object-cover rounded-xl object-center"
             />
-            <div className="flex flex-row justify-between h-24">
+            <div className="grid grid-cols-4 gap-4 w-full overflow-hidden">
               <img
                 src={images.img1}
                 alt=""
-                className="w-24 h-24 rounded-md cursor-pointer"
+                className="w-24 h-24 rounded-md cursor-pointer aspect-square object-cover"
                 onClick={() => setActiveImage(images.img1)}
               />
               <img
                 src={images.img2}
                 alt=""
-                className="w-24 h-24 rounded-md cursor-pointer"
+                className="w-24 h-24 rounded-md cursor-pointer oaspect-square object-cover"
                 onClick={() => setActiveImage(images.img2)}
               />
               <img
                 src={images.img3}
                 alt=""
-                className="w-24 h-24 rounded-md cursor-pointer"
+                className="w-24 h-24 rounded-md cursor-pointer aspect-square object-cover"
                 onClick={() => setActiveImage(images.img3)}
               />
               <img
                 src={images.img4}
                 alt=""
-                className="w-24 h-24 rounded-md cursor-pointer"
+                className="w-24 h-24 rounded-md cursor-pointer aspect-square object-cover"
                 onClick={() => setActiveImage(images.img4)}
               />
             </div>
@@ -60,9 +60,9 @@ const ProductDes = () => {
           <div className="flex flex-col gap-4 lg:w-2/4">
             <div>
               <span className=" text-[#0052D4] font-semibold">
-                Special Sneaker
+                2023 edition
               </span>
-              <h1 className="text-3xl font-bold text-[#6FB1FC]">Nike Invincible 3</h1>
+              <h1 className="text-3xl font-bold text-[#6FB1FC]">Macbook Pro</h1>
             </div>
             <p className="text-gray-700">
               Con un'ammortizzazione incredibile per sostenerti in tutti i tuoi
